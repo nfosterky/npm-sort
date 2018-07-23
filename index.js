44176eaababd7edca87d9ed9731e63fa046a5ee9
Nathaniel Foster <nathanielsmusic@gmail.com>
2018-07-23 10:38:27 -0600

    Add super fast sort

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const bubbleSort = (array) => {
   return array;
 };
 
+const superFastSort = (array) => {
+  return array.sort();
+}
+
 module.exports = {
-  bubbleSort
+  bubbleSort,
+  superFastSort
 };
